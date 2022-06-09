3fde40a267648f3998e89e9368334814479ec5b9
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-06-09 16:13:32 +0600

    about us updated

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text() {
 	var animateSpan = jQuery('.arlo_tm_animation_text_word');
 
 	animateSpan.typed({
-		strings: ["Full Stack Developer", "Mern Stack Developer", "ReactJs"],
+		strings: ["Full Stack Developer", "Mern Stack Developer", "ReactJs Developer"],
 		loop: true,
 		startDelay: 1e3,
 		backDelay: 2e3
